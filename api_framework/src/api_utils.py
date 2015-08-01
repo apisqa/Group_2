39a62afda1b506e8c6b20d50ac406de8f515e36a
frost <fxtrdnsk@gmail.com>
2015-08-01 12:02:22 -0700

    first testcase

--- api_framework/src/api_utils.py
+++ api_framework/src/api_utils.py
@@ -14,6 +14,43 @@ class Config:
         self.domain = self.parser.get('Server', 'domain')
         self.admin_login = self.parser.get('Server', 'admin')
         self.password = self.parser.get('Server', 'password')
+        self.test_path = self.parser.get('Server', 'testpath')
 
-f = Config()
-print(f.admin_login)
+
+class Calls:
+    def __init__(self):
+        self.config = Config()
+
+    def create_folder(self, name, domain=None, username=None, password=None,
+                      content_type=None, accept=None, method=None, test_path=None):
+        if domain is None:
+            domain = self.config.domain
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+        if method is None:
+            method = 'POST'
+        if accept is None:
+            accept = 'application/json'
+        if content_type is None:
+            content_type = 'application/json'
+        if test_path is None:
+            test_path = self.config.test_path
+
+        endpoint = '/public-api/v1/fs'
+        url = domain + endpoint + test_path + name
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        data = dict()
+        data['action'] = 'add_folder'
+        data = json.dumps(data)
+        r = requests.request(
+            url=url,
+            auth=(username, password),
+            headers=headers,
+            data=data,
+            method=method
+        )
+        return r
--- api_framework/tests/testCreateFolder.py
+++ api_framework/tests/testCreateFolder.py
@@ -1,49 +1,14 @@
-import requests
-import json
+from api_utils import Calls
+from unittest import TestCase
+import httplib
 
 
-def create_folder(name, domain=None, username=None, password=None,
-                  content_type=None, accept=None, method=None, test_path=None):
-    if domain is None:
-        domain = 'https://istepanko.qa-egnyte.com'
-    if content_type is None:
-        content_type = 'application/json'
-    if username is None:
-        username = 'istepanko'
-    if password is None:
-        password = 'apitest2'
-    if method is None:
-        method = 'POST'
-    if accept is None:
-        accept = 'application/json'
-    if test_path is None:
-        test_path = '/Shared/smoke_test/'
+class TestClass(TestCase):
 
-    endpoint = '/public-api/v1/fs'
-    url = domain + endpoint + test_path + name
-    headers = dict()
-    headers['Content-Type'] = content_type
-    headers['Accept'] = accept
-    data = dict()
-    data['action'] = 'add_folder'
-    data = json.dumps(data)
-    r = requests.request(
-        url=url,
-        auth=(username, password),
-        headers=headers,
-        data=data,
-        method=method
-    )
-    return r
+    @classmethod
+    def setUpClass(cls):
+        cls.calls = Calls()
 
-for i in range(1000):
-    resp = create_folder(name='test%s' % i)
-    print('\n'
-          'Folder %s created!\n'
-          'Data:'
-          '\n'
-          '%s'
-          '\n'
-          'Status_code:'
-          '\n'
-          '%s' % (resp.content, resp.status_code, i))
+    def test_create_folder_positive(self):
+        resp = self.calls.create_folder('test_folder1')
+        assert resp.status_code == httplib.CREATED
