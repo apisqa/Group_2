3b022f9e945d0bc9e7d2e1e10790b82016232286
frost <fxtrdnsk@gmail.com>
2015-08-01 13:24:25 -0700

    unauthorized test case

--- api_framework/tests/testCreateFolder.py
+++ api_framework/tests/testCreateFolder.py
@@ -12,10 +12,12 @@ class TestClass(TestCase):
     def test_create_folder_positive(self):
         folder = self.calls.gen_random_name()
         resp = self.calls.create_folder(folder)
-        assert resp.status_code == httplib.CREATED
+        assert resp.http_code == httplib.CREATED
 
     def test_create_folder_incorrect_credentials(self):
         folder = self.calls.gen_random_name()
         resp = self.calls.create_folder(folder, password='asdas')
-        assert resp.status_code == httplib.UNAUTHORIZED
-        assert resp.content['inputErrors']['credentials'] == 1
+        assert resp.http_code == httplib.UNAUTHORIZED
+        assert resp.json['inputErrors']['credentials'][0]['code'] == 'INVALID_CREDENTIALS'
+        assert resp.json['inputErrors']['credentials'][0]['msg'] == 'his request is unauthenticated. Please provide ' \
+                                                                    'credentials and try again.'
