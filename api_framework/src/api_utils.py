96faf6c1c5b4815339325c49c6bc0edee68dce16
frost <fxtrdnsk@gmail.com>
2015-08-05 21:59:58 -0700

    UPDATE 2.4.3

--- api_framework/src/api_utils.py
+++ api_framework/src/api_utils.py
@@ -3,34 +3,44 @@ import json
 from ConfigParser import SafeConfigParser
 import time
 import os
+import sys
 
 
 class Config:
     def __init__(self):
+        """Creating object of SafeConfigParser() class to get access to it's methods and attributes"""
         self.parser = SafeConfigParser()
+        """Checking if config file exists"""
         if os.path.isfile('config.ini'):
             self.parser.read('config.ini')
         else:
             print('No config.ini found under root folder.')
+            # Shut down Python
+            sys.exit()
+        # Creating variables with values from config.ini
         self.domain = self.parser.get('Server', 'domain')
         self.admin_login = self.parser.get('Server', 'admin')
         self.password = self.parser.get('Server', 'password')
         self.test_path = self.parser.get('Server', 'testpath')
 
 
+# Define class for sexy looking response object
 class Response:
     def __init__(self):
-        self.http_code = 0
-        self.json = dict()
+        self.http_code = None
+        self.body = dict()
         self.headers = dict()
 
 
+# Define class to store all methods we need for framework
 class Calls:
     def __init__(self):
+        # Creating object of class Config()
         self.config = Config()
+        # Creating variable with string to return it in case there is no json
         self.no_json = 'noJson'
 
-    def create_folder(self, name, domain=None, username=None, password=None,
+    def create_folder(self, folder_name, domain=None, username=None, password=None,
                       content_type=None, accept=None, method=None, test_path=None):
         if domain is None:
             domain = self.config.domain
@@ -48,13 +58,14 @@ class Calls:
             test_path = self.config.test_path
 
         endpoint = '/public-api/v1/fs'
-        url = domain + endpoint + test_path + name
+        url = domain + endpoint + test_path + folder_name
         headers = dict()
         headers['Content-Type'] = content_type
         headers['Accept'] = accept
         data = dict()
         data['action'] = 'add_folder'
         data = json.dumps(data)
+        # Request method returning server response to r
         r = requests.request(
             url=url,
             auth=(username, password),
@@ -63,22 +74,26 @@ class Calls:
             method=method
         )
 
+        # Fist: Trying to parse json and convert it to Pythonic dict.
         try:
             json_resp = json.loads(r.content)
         except ValueError:
+            # If parsing failed then check, maybe method was 'OPTIONS'
             if method == 'OPTIONS':
                 json_resp = r.content
+            # And finally return no_json string to json_resp variable
             else:
                 json_resp = self.no_json
-
+        # Putting processed r.content into r.json
         r.json = json_resp
+        # Creating object of class Response()
         response = Response()
         response.http_code = r.status_code
-        response.json = r.json
+        response.body = r.json
         response.headers = r.headers
         return response
 
-    def delete_folder(self, name, domain=None, username=None, password=None,
+    def delete_folder(self, folder_name, domain=None, username=None, password=None,
                       content_type=None, accept=None, method=None, test_path=None):
         if domain is None:
             domain = self.config.domain
@@ -96,7 +111,7 @@ class Calls:
             test_path = self.config.test_path
 
         endpoint = '/public-api/v1/fs'
-        url = domain + endpoint + test_path + name
+        url = domain + endpoint + test_path + folder_name
         headers = dict()
         headers['Content-Type'] = content_type
         headers['Accept'] = accept
@@ -118,10 +133,11 @@ class Calls:
         r.json = json_resp
         response = Response()
         response.http_code = r.status_code
-        response.json = r.json
+        response.body = r.json
         response.headers = r.headers
         return response
 
+    # Defining static(nothing to do with parent class) method, which generates random
     @staticmethod
     def gen_random_name():
         return 'dynamic_name_%s' % str(time.time()).replace('.', '')
--- api_framework/tests/testCreateFolder.py
+++ api_framework/tests/testCreateFolder.py
@@ -3,21 +3,36 @@ from unittest import TestCase
 import httplib
 
 
+# Defining TestClass class using inheritance from TestCase class of module unittest
 class TestClass(TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.calls = Calls()
+        print('setUpClass executed')
 
     def test_create_folder_positive(self):
+        folder_name = self.calls.gen_random_name()
+        resp = self.calls.create_folder(folder_name)
+        assert resp.http_code == httplib.CREATED
+        self.calls.delete_folder(folder_name)
+
+    def test_delete_folder(self):
         folder = self.calls.gen_random_name()
         resp = self.calls.create_folder(folder)
         assert resp.http_code == httplib.CREATED
+        resp = self.calls.delete_folder(folder)
+        assert resp.http_code == httplib.OK
 
     def test_create_folder_incorrect_credentials(self):
         folder = self.calls.gen_random_name()
         resp = self.calls.create_folder(folder, password='asdas')
         assert resp.http_code == httplib.UNAUTHORIZED
-        assert resp.json['inputErrors']['credentials'][0]['code'] == 'INVALID_CREDENTIALS'
-        assert resp.json['inputErrors']['credentials'][0]['msg'] == 'This request is unauthenticated. Please provide ' \
+        assert resp.body['inputErrors']['credentials'][0]['code'] == 'INVALID_CREDENTIALS'
+        assert resp.body['inputErrors']['credentials'][0]['msg'] == 'This request is unauthenticated. Please provide ' \
                                                                     'credentials and try again.'
+
+    def test_delete_non_existent_folder(self):
+        folder_name = self.calls.gen_random_name()
+        resp = self.calls.delete_folder(folder_name)
+        assert resp.http_code == httplib.NOT_FOUND
